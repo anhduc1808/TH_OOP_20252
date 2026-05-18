043f7af552a2f344ed29cde689189e30cac78782
Anh Duc <anhducnguyensy06@gmail.com>
2026-05-18 16:28:17 +0700

    feature/aims-project/polymorphism-with-toString-method

--- Lab04/src/hust/soict/dsai/aims/test/CartToStringTest.java
+++ Lab04/src/hust/soict/dsai/aims/test/CartToStringTest.java
@@ -3,30 +3,25 @@ package hust.soict.dsai.aims.test;
 import hust.soict.dsai.aims.cart.Cart;
 import hust.soict.dsai.aims.media.Book;
 import hust.soict.dsai.aims.media.CompactDisc;
-import hust.soict.dsai.aims.media.DigitalVideoDisc;
 import hust.soict.dsai.aims.media.Track;
+import hust.soict.dsai.aims.media.DigitalVideoDisc;
 
-public class CartTest {
+public class CartToStringTest {
     public static void main(String[] args) {
         Cart cart = new Cart();
 
-        DigitalVideoDisc dvd1 = new DigitalVideoDisc(1, "DVD Title", "DVD", "Director 1", 10, 5.0f);
-        DigitalVideoDisc dvd2 = new DigitalVideoDisc(2, "DVD Title", "DVD", "Director 2", 12, 5.0f);
-        cart.addMedia(dvd1);
-        cart.addMedia(dvd2);
-
-        Book book = new Book(3, "Java Basics", "Book", 25.0f);
+        Book book = new Book(1, "Java Basics", "Book", 25.0f);
         book.addAuthor("James Gosling");
         book.addAuthor("Joshua Bloch");
 
         Track track1 = new Track("Track 1", 180);
         Track track2 = new Track("Track 2", 210);
 
-        CompactDisc cd = new CompactDisc(4, "Best Hits", "Music", "Artist A", "Director A", 30.0f);
+        CompactDisc cd = new CompactDisc(2, "Best Hits", "Music", "Artist A", "Director A", 30.0f);
         cd.addTrack(track1);
         cd.addTrack(track2);
 
-        DigitalVideoDisc dvd = new DigitalVideoDisc(5, "Tutorial DVD", "DVD", "Director DVD", 120, 40.0f);
+        DigitalVideoDisc dvd = new DigitalVideoDisc(3, "Tutorial DVD", "DVD", "Director DVD", 120, 40.0f);
 
         cart.addMedia(book);
         cart.addMedia(cd);
@@ -35,5 +30,4 @@ public class CartTest {
         cart.print();
     }
 }
-
 
